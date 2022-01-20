670fb15e3d6af67644f6304a71b8c9b1a06aca19
Kazuki <kazuki19951215@gmail.com>
2022-01-20 11:15:47 +0900

    プレイヤーにもモデル導入、和風テイストの世界観で忍者同士のバトル。アニメーションが動きと連動せずにモデルと大きくずれる

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -13,10 +13,16 @@ public class PlayerMove : MonoBehaviour
     private GameObject enemy;
     private EnemyMove enemyMove;
 
+    [SerializeField]
+    private Animator playerAnim;
+
+    [SerializeField]
+    private GameObject playerModel;
+
     // Start is called before the first frame update
     void Start()
     {
-        this.transform.position = new Vector3(0, 1, 0);
+        this.transform.position = new Vector3(0, 0, 0);
         enemyMove = enemy.GetComponent<EnemyMove>();
     }
 
@@ -31,8 +37,11 @@ public class PlayerMove : MonoBehaviour
             case "TopMoveButton":
                 if (this.transform.position.z > -1.5f)
                 {
+                    playerAnim.SetTrigger("ForwardTrigger");
                     Tween tween = transform.DOMove(new Vector3(0, 0, -0.5f), 3f).SetRelative();
                     tween.SetEase(Ease.Linear);
+                    playerModel.transform.DOMove(new Vector3(0, 0, 2), 3f).SetRelative();
+
                     // this.transform.position = new Vector3(this.transform.position.x, positionY, this.transform.position.z - posChange);
                 }
                 break;
@@ -40,8 +49,10 @@ public class PlayerMove : MonoBehaviour
             case "DownMoveButton":
                 if (this.transform.position.z < 0)
                 {
+                    playerAnim.SetTrigger("BackTrigger");
                     Tween tween = transform.DOMove(new Vector3(0, 0, 0.5f), 3f).SetRelative();
                     tween.SetEase(Ease.Linear);
+                    playerModel.transform.DOMove(new Vector3(0, 0, -2), 3f).SetRelative();
                     // this.transform.position = new Vector3(this.transform.position.x, positionY, this.transform.position.z + posChange);
                 }
                 break;
@@ -49,8 +60,10 @@ public class PlayerMove : MonoBehaviour
             case "LeftMoveButton":
                 if (this.transform.position.x < 0)
                 {
+                    playerAnim.SetTrigger("LeftTrigger");
                     Tween tween = transform.DOMove(new Vector3(0.5f, 0, 0), 3f).SetRelative();
                     tween.SetEase(Ease.Linear);
+                    playerModel.transform.DOMove(new Vector3(-2, 0, 0), 3f).SetRelative();
                     // this.transform.position = new Vector3(this.transform.position.x + posChange, positionY, this.transform.position.z);
                 }
                 break;
@@ -58,8 +71,10 @@ public class PlayerMove : MonoBehaviour
             case "RightMoveButton":
                 if (this.transform.position.x > -1.5f)
                 {
+                    playerAnim.SetTrigger("RightTrigger");
                     Tween tween = transform.DOMove(new Vector3(-0.5f, 0, 0), 3f).SetRelative();
                     tween.SetEase(Ease.Linear);
+                    playerModel.transform.DOMove(new Vector3(2, 0, 0), 3f).SetRelative();
                     // this.transform.position = new Vector3(this.transform.position.x - posChange, positionY, this.transform.position.z);
                 }
                 break;
