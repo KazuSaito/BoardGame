87e673371a3bb2d5a5872bae38e61a381c193916
Kazuki <kazuki19951215@gmail.com>
2022-02-15 17:01:46 +0900

    カウントダウン式のターン制御に変更、指定ターンを迎えると負けの設定追加

--- Assets/Scripts/Exp UI/PhaseManager.cs
+++ Assets/Scripts/Exp UI/PhaseManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class PhaseManager : MonoBehaviour
@@ -11,8 +12,8 @@ public class PhaseManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        phaseCount = 1;
-        phaseText.text = phaseCount + "ターン目";
+        phaseCount = 10;
+        phaseText.text = "残り" + phaseCount + "ターン";
     }
 
     public void PhaseCount()
@@ -23,7 +24,29 @@ public class PhaseManager : MonoBehaviour
     IEnumerator PhaseShowDelay()
     {
         yield return new WaitForSeconds(5f);
-        phaseCount++;
-        phaseText.text = phaseCount + "ターン目";
+        phaseCount-=1;
+        if (phaseCount > 0)
+        {
+            phaseText.text = "残り" + phaseCount + "ターン";
+        }
+        else
+        {
+            yield return new WaitForSeconds(3f);
+            GameObject canvas = GameObject.FindWithTag("Canvas");
+            canvas.GetComponent<CanvasManager>().Lose();
+            StartCoroutine("LoadEndingScene");
+        }
+        
+        
     }
+
+    private IEnumerator LoadEndingScene()
+    {
+        yield return new WaitForSeconds(4f);
+        SceneManager.LoadScene("Ending");
+    }
+
+
+
+
 }
